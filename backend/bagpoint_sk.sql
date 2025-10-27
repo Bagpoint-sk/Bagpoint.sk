eca9571d6fe52be53b64c8b1bc15dee47f79a073
igorko <igxxd@proton.me>
2025-10-27 21:09:40 +0100

    nova struktura (backend/frontend/docs)

---
+++ backend/bagpoint_sk.sql
@@ -0,0 +1,44 @@
+-- Original : Oracle SQL Developer Data Modeler
+-- prepis: do MySQL
+-- zmeny: int auto_increment, varchar, decimal, datetime
+-- CHECK -> nahradeny ENUM 
+
+CREATE TABLE boxes (
+    box_id INT AUTO_INCREMENT PRIMARY KEY,
+    box_name VARCHAR(10) NOT NULL,
+    size ENUM('S','M','L') NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_per_day DECIMAL(10,2) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+CREATE TABLE reservations (
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+    users_user_id INT NOT NULL,
+    boxes_box_id INT NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    reservation_date DATETIME NOT NULL,
+    status ENUM('pending', 'confirmed','cancelled','completed') NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY (users_user_id) REFERENCES users(user_id),
+    FOREIGN KEY (boxes_box_id) REFERENCES boxes(box_id)
+);
+
+CREATE TABLE payments (
+    payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    reservation_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date DATETIME NOT NULL,
+    payment_method ENUM('transfer', 'card') NOT NULL,
+    payment_status ENUM('pending','paid','cancelled') NOT NULL,
+    FOREIGN KEY (reservation_id) REFERENCES reservations(reservation_id)
+);
